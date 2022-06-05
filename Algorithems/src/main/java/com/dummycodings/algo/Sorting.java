8e9018d2f0c174652ef90560e77b508304632179
Karthick Raj.R <kraj4000@gmail.com>
2022-06-06 03:45:51 +0530

    basic algoritham project created

---
+++ Algorithems/src/main/java/com/dummycodings/algo/Sorting.java
@@ -0,0 +1,21 @@
+/*
+ * This code belongs to Dummy Codings
+ * 
+ * 
+ */
+package com.dummycodings.algo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author KarthickRaj.R at DummyCodings.com
+ */
+public class Sorting {
+
+    public static void main(String[] args) {
+        List<Integer> randomNum=Arrays.asList(12,34,1,7,4,10,45,23);
+    }
+
+}
